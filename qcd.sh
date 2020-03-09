e8554a33c3314df3f1e6319324c299f5ff06c6b1
nalinahuja22 <nalinahuja22@gmail.com>
2020-03-09 14:14:01 -0400

    Project Backup

--- qcd.sh
+++ qcd.sh
@@ -1,36 +1,42 @@
 #!/usr/bin/env bash
 
 function qcd() {
-  # Error Check
-  if [[ -z $1 ]]
+  # Store First Arg
+  indicated_dir=$1
+
+  if [[ -z $indicated_dir ]]
   then
-    echo -e "qcd: No directory specified."
-  else
-    # Store First Arg
-    indicated_dir=$1
+    indicated_dir=~
+  fi
 
-    # Create Dir Store
-    if [[ ! -f ~/dev/qcd/.qcd_store ]]
+  # Create Dir Store
+  if [[ ! -f ~/dev/qcd/.qcd_store ]]
+  then
+    touch ~/dev/qcd/.qcd_store
+  fi
+
+  # Is Valid Directory
+  if [[ -e $indicated_dir ]]
+  then
+    echo "Valid"
+    command cd $indicated_dir
+
+    new_dir=$(pwd -P)
+    new_ept=$(basename $new_dir)
+
+    if [[ -z $(egrep -s -m1 -x "$new_ept.*" ~/dev/qcd/.qcd_store) ]]
     then
-      touch ~/dev/qcd/.qcd_store
+      printf "%s %s\n" $new_ept $new_dir >> ~/dev/qcd/.qcd_store
     fi
+  else
+    match=$(egrep -s -m1 -x "$indicated_dir.*" ~/dev/qcd/.qcd_store | cut -d ' ' -f2)
 
-    # Is Valid Directory
-    if [[ -e $indicated_dir ]]
+    if [[ -z $match ]]
     then
-      echo "Valid"
-      command cd $indicated_dir
-      pwd -P >> ~/dev/qcd/.qcd_store
+      echo -e "qcd: Cannot match keyword to directory."
     else
-      match=$(egrep -s -m1 "$indicated_dir" ~/dev/qcd/.qcd_store)
-
-      if [[ -z $match ]]
-      then
-        echo -e "qcd: Cannot match keyword to directory."
-      else
-        echo "Linked"
-        command cd $match
-      fi
+      echo "Linked"
+      command cd $match
     fi
   fi
 }
