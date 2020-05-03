ade51c4931295db6f1367c683487597b4d083b00
nalinahuja22 <nalinahuja22@gmail.com>
2020-05-02 22:47:50 -0400

    Project Backup

--- qcd.sh
+++ qcd.sh
@@ -70,7 +70,7 @@ function qcd() {
   if [[ "$1" = "$HELP" ]]
   then
     # Print Help
-    printf "${b}Qcd Usage${n}\n\n  Help:\n    qcd -h\t\t\tShow Usage\n\n"
+    printf "${b}QCD Usage${n}\n\n  Help:\n    qcd -h\t\t\tShow Usage\n\n"
     printf "  Change Directories:\n    qcd [path]\t\t\tChange To Valid Path\n    qcd [link]/[subdir]\t\tChange To Linked Path With Opt. Subdir\n\n"
     printf "  Link Management:\n    qcd -c\t\t\tCleanup Store File\n    qcd -f\t\t\tForget Current Directory\n    qcd [link] -f\t\tForget Symbolic Link\n\n"
 
