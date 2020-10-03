b39a359c060b7a6faedae7308ff405f270c38e56
Nalin Ahuja <nalinahuja22@gmail.com>
2020-10-03 12:06:05 -0400

    Project Backup

--- qcd.sh
+++ qcd.sh
@@ -905,8 +905,32 @@ function qcd() {
       # List Matching Paths
       if [[ -z ${mpath} && ! -z ${fpaths} ]]
       then
-        # Generate Prompt
-        command echo -e "qcd: Multiple paths linked to ${B}${dir_arg%/}${N}"
+        # Display Prompt
+        command echo -en "\rqcd: Generating option list..."
+
+        # Check For Link Wildcarding
+        if [[ ! -z ${wlink} ]]
+        then
+          # Initialize Array Representation
+          local flist=${ESTR}
+
+          # Iterate Over Filtered Paths
+          for fpath in ${fpaths[@]}
+          do
+            # Form String Representation
+            flist="${flist}${fpath}\n"
+          done
+
+          # Reset Filtered Paths
+          fpaths=()
+
+          # Rank Paths By Proximity Match
+          fpaths+=($(command echo -e "${flist}" | command egrep -s -x ".*/${wlink}/$"))
+          fpaths+=($(command echo -e "${flist}" | command egrep -s -x -v ".*/${wlink}/$"))
+        fi
+
+        # Display Prompt
+        command echo -e "\rqcd: Multiple paths linked to ${B}${dir_arg%/}${N}"
 
         # Generate Menu
         _display_menu ${fpaths[@]}
