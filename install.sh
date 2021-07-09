79e570d5bb033a046fd6b9d89a5d49a1f452aa32
Nalin Ahuja <nalinahuja22@gmail.com>
2021-07-09 17:09:40 -0400

    Project Backup

--- install.sh
+++ install.sh
@@ -9,18 +9,17 @@ readonly QCD_FOLD=~/.qcd
 
 # Program Files
 readonly QCD_SH=./qcd.sh
-readonly QCD_PL=./lcs.pl
+readonly QCD_PL=./rank.pl
 
 # Release Files
 readonly QCD_LICENSE=./LICENSE
 readonly QCD_VERSION=./VERSION
 readonly QCD_READ_ME=./README.md
 
-# Terminal Profiles
-readonly BASHRC=~/.bashrc
-readonly BASHPR=~/.bash_profile
+# Bash Configurations
+readonly BASH_CONFIGS=(~/.bashrc ~/.bash_profile)
 
-# Verify Program Files
+# Verify Installation Files
 if [[ ! -e "${QCD_SH}" || ! -e "${QCD_PL}" || ! -e "${QCD_LICENSE}" || ! -e "${QCD_VERSION}" || ! -e "${QCD_READ_ME}" ]]
 then
   # Display Prompt
@@ -39,12 +38,12 @@ readonly TRUE=1 FALSE=0
 readonly NL="\n" YES="y" ESEQ=$(command printf "\033") &> /dev/null
 
 # Text Formatting Strings
-readonly B=$(command printf "${ESEQ}[1m") W=$(command printf "${ESEQ}[30m${ESEQ}[47m") N=$(command printf "${ESEQ}(B${ESEQ}[m") &> /dev/null
+readonly B=$(command printf "${ESEQ}[1m") N=$(command printf "${ESEQ}(B${ESEQ}[m") &> /dev/null
 
 # End Embedded Constants----------------------------------------------------------------------------------------------------------------------------------------------
 
-# Upgrade Status
-declare UPGRADE_STATUS=${TRUE}
+# Update Status
+declare UPDATE_STATUS=${TRUE}
 
 # Installation Status
 declare INSTALL_STATUS=${FALSE}
@@ -54,36 +53,29 @@ declare INSTALL_VERSION=$(command cat ${QCD_VERSION} 2> /dev/null)
 
 # End Global Variables------------------------------------------------------------------------------------------------------------------------------------------------
 
-# Prompt User For Installation Confirmation
-command read -p "qcd: Confirm installation ${B}${INSTALL_VERSION}${N} [y/n]: " confirm
+# Prompt User For Program Installation Confirmation
+command read -p "qcd: Confirm program installation ${B}${INSTALL_VERSION}${N} [y/n]: " confirm
 
 # Determine Action
 if [[ "${confirm,}" == "${YES}" ]]
 then
-  # Add Command To Terminal Profile
+  # Add Command To Bash Configurations
   if [[ ! -d "${QCD_FOLD}" ]]
   then
-    # Find ~/.bashrc
-    if [[ -f "${BASHRC}" ]]
-    then
-      # Add Command To ~/.bashrc
-      command echo -e "${NL}# QCD Utility Source${NL}command source ~/.qcd/qcd.sh${NL}" >> ${BASHRC}
+    # Iterate Over Bash Configurations
+    for BASH_CONFIG in ${BASH_CONFIGS[@]}; do
+      # Verfiy Bash Configuration Exists
+      if [[ -f "${BASH_CONFIG}" ]]
+      then
+        # Add Command To Bash Configuration
+        command echo -e "${NL}# QCD Utility Source${NL}command source ~/.qcd/qcd.sh${NL}" >> ${BASH_CONFIG}
 
-      # Update Installation Status
-      INSTALL_STATUS=${TRUE}
-    fi
+        # Update Installation Status
+        INSTALL_STATUS=${TRUE}
+      fi
+    done
 
-    # Find ~/.bash_profile
-    if [[ -f "${BASHPR}" ]]
-    then
-      # Add Command To ~/.bash_profile
-      command echo -e "${NL}# QCD Utility Source${NL}command source ~/.qcd/qcd.sh${NL}" >> ${BASHPR}
-
-      # Update Installation Status
-      INSTALL_STATUS=${TRUE}
-    fi
-
-    # Check Installation Status
+    # Verify Installation Status
     if [[ ${INSTALL_STATUS} -eq ${FALSE} ]]
     then
       # Display Error Prompt
@@ -93,12 +85,19 @@ then
       command exit 1
     fi
 
-    # Update Upgrade Status
-    UPGRADE_STATUS=${FALSE}
+    # Change Update Status
+    UPDATE_STATUS=${FALSE}
   fi
 
-  # Create Program Folder
-  command mkdir ${QCD_FOLD} 2> /dev/null
+  # Determine Appropriate Action
+  if [[ ${UPDATE_STATUS} -eq ${TRUE} ]]
+  then
+    # Uninstall Old Program Files
+    command rm ${QCD_FOLD}/*.sh ${QCD_FOLD}/*.pl
+  else
+    # Create Program Folder
+    command mkdir ${QCD_FOLD} 2> /dev/null
+  fi
 
   # Install QCD Program Files
   command mv ${QCD_SH} ${QCD_FOLD} 2> /dev/null
@@ -110,9 +109,9 @@ then
   command mv ${QCD_READ_ME} ${QCD_FOLD} 2> /dev/null
 
   # Determine Appropriate Prompt
-  if [[ ${UPGRADE_STATUS} -eq ${TRUE} ]]
+  if [[ ${UPDATE_STATUS} -eq ${TRUE} ]]
   then
-    # Display Upgrade Prompt
+    # Display Update Prompt
     command echo -e "→ Upgraded QCD to ${B}${INSTALL_VERSION}${N}"
   else
     # Display Installation Prompt
@@ -126,4 +125,4 @@ else
   command echo -e "→ Installation aborted"
 fi
 
-# End QCD Installation------------------------------------------------------------------------------------------------------------------------------------------------
+# End QCD Installation Script-----------------------------------------------------------------------------------------------------------------------------------------
